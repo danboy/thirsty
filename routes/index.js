5b6ddc8e76a542f1c992f16591d16874488e11fa
Dan Nawara <dan@pinmonkey.com>
2012-05-04 13:03:21 -0500

    Started the models

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,29 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/beer');
 
+var Schema = mongoose.Schema;
+
+var Drinks = new Schema({
+  type: { type: String, required: true}
+, from: { type: String, requred: true }
+});
+
+var User = new Schema({  
+  name: { type: String, required: true}
+, drinks: [Drinks]
+, lastDrink: {type: Date, default: Date.now}
+
+});
+
+var User = mongoose.model('User', User);
 /*
  * GET home page.
  */
 var Beer = {
   index: function(req, res){
-    res.render('index', { title: 'Drinks owed.' })
+    User.find({}, function(err, users){
+      res.send({ title: 'Drinks owed.' });
+    });
   }
 };
 
