a9a813c353edc161fe85eac159f2ffbce5f9406e
Dan Nawara <dnawara@groupon.com>
2012-05-05 15:34:37 -0500

    its alive

--- app.js
+++ app.js
@@ -4,7 +4,24 @@
  */
 
 var express = require('express')
-  , routes = require('./routes');
+  , mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/beer');
+
+var Schema = mongoose.Schema;
+
+var Drinks = new Schema({
+  type: { type: String, required: true}
+, from: { type: String, requred: true }
+});
+
+var User = new Schema({  
+  name: { type: String, required: true, unique: true}
+, drinks: [Drinks]
+, lastDrink: {type: Date, default: Date.now}
+
+});
+
+var User = mongoose.model('User', User);
 
 var app = module.exports = express.createServer();
 
@@ -27,9 +44,75 @@ app.configure('production', function(){
   app.use(express.errorHandler());
 });
 
+
+var checkForDrinks= function(req,res,next){
+  User.findOne({name: req.body.drink.from}, function(er, pal){
+    if(er) throw er;
+    console.log(pal);
+    if(pal == null){
+      next(new Error("That's not a user"));
+    }
+    var hasDrinksToGive = function(){
+      var today = new Date();
+      var lastbeer = new Date(pal.lastDrink);
+      console.log(lastbeer, today);
+      if(lastbeer.getMonth() == today.getMonth() && lastbeer.getDate() == today.getDate()){
+        return false;
+      }else{
+        return true;
+      }
+    }
+    if(pal && hasDrinksToGive()){
+      pal.lastDrink = new Date();
+      pal.save(function(er){
+        next();
+      });
+    }else{
+      next(new Error('User Has no drinks to give.'));
+    }
+  });
+}
+
 // Routes
 
-app.get('/', routes.index);
+app.get('/', function(req, res){
+  User.find({}, function(err, users){
+    res.render('index', { title: 'Drinks owed.', users: users });
+  });
+});
 
+app.post('/users/', checkForDrinks, function(req, res){
+  if(req.body.drink){
+    User.findOne({name: req.body.drink.to}, function(err, user){
+      if(err) throw err;
+      if(user){
+        user.drinks.push({type: req.body.drink.type, from: req.body.drink.from});
+        user.save(function(err){
+        if(err) throw err;
+          res.send(user);
+        });
+      }else{
+        var d = new Date();
+        d.setDate(d.getDate()-1);
+        user = new User({name: req.body.drink.to, lastDrink: d});
+        user.drinks.push({type: req.body.drink.type, from: req.body.drink.from});
+        user.save(function(err){
+        if(err) throw err;
+          res.send(user);
+        });
+      }
+    });
+  }else{
+    res.send({'error': 'looks like you\'re missing some stuff'})
+  }
+});
+app.post('/dan',function(req, res){
+  dan = new User({name: 'Dan'});
+  dan.save(function(er){
+    if(!er){
+      res.send("created dan\n")
+    }
+  });
+})
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- routes/index.js
+++ routes/index.js
@@ -22,9 +22,34 @@ var User = mongoose.model('User', User);
 var Beer = {
   index: function(req, res){
     User.find({}, function(err, users){
-      res.send({ title: 'Drinks owed.' });
+      res.send({ title: 'Drinks owed.', users: users });
     });
+  }
+, create: function(req, res){
+    if(req.body.drink){
+        User.findOne({name: req.body.drink.to}, function(err, user){
+          if(err) throw err;
+          if(user){
+            //user.drinks
+            user.drinks.push({type: req.body.drink.type, from: req.body.drink.from});
+            user.save(function(err){
+            if(err) throw err;
+              res.send(user);
+            });
+          }else{
+            user = new User({name: req.body.drink.to});
+            user.drinks.push({type: req.body.drink.type, from: req.body.drink.from});
+            user.save(function(err){
+            if(err) throw err;
+              res.send(user);
+            });
+          }
+        });
+    }else{
+      res.send({'error': 'looks like you\'re missing some stuff'})
+    }
   }
 };
 
 module.exports= Beer;
+
