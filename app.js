9538a4b340f546635fdfc571d4a206a5f7c0a32f
Dan Nawara <dnawara@groupon.com>
2012-05-06 09:14:23 -0500

    added email to the model, still messy but a little cleanup

--- app.js
+++ app.js
@@ -1,29 +1,10 @@
-
 /**
  * Module dependencies.
  */
 
 var express = require('express')
-  , mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/beer');
-
-var Schema = mongoose.Schema;
-
-var Drinks = new Schema({
-  type: { type: String, required: true}
-, from: { type: String, requred: true }
-});
-
-var User = new Schema({  
-  name: { type: String, required: true, unique: true}
-, drinks: [Drinks]
-, lastDrink: {type: Date, default: Date.now}
-
-});
-
-var User = mongoose.model('User', User);
-
-var app = module.exports = express.createServer();
+  , users = require('./routes/users')
+  , app = module.exports = express.createServer();
 
 // Configuration
 
@@ -46,85 +27,20 @@ app.configure('production', function(){
 
 
 var checkForDrinks= function(req,res,next){
-  User.findOne({name: req.body.drink.from}, function(er, pal){
-    if(er) throw er;
-    console.log(pal);
-    if(pal == null){
-      next(new Error("That's not a user"));
-    }
-    var hasDrinksToGive = function(){
-      var today = new Date();
-      var lastbeer = new Date(pal.lastDrink);
-      console.log(lastbeer, today);
-      if(lastbeer.getMonth() == today.getMonth() && lastbeer.getDate() == today.getDate()){
-        return false;
-      }else{
-        return true;
-      }
-    }
-    if(pal && hasDrinksToGive()){
-      pal.lastDrink = new Date();
-      pal.save(function(er){
-        next();
-      });
-    }else{
-      next(new Error('User Has no drinks to give.'));
-    }
-  });
 }
 
 // Routes
+console.log(users.list);
+app.get('/', users.index);
 
-app.get('/', function(req, res){
-  User.find({}, function(err, users){
-    res.render('index', { title: 'Drinks owed.', users: users });
-  });
-});
-app.get('/users', function(req, res){
-  User.find({}, function(err, users){
-    res.send(users);
-  });
-});
 
-app.get('/users/:name', function(req, res){
-  User.findOne({name: req.params.name}, function(err, user){
-    res.send(user);
-  });
-});
+app.get('/recent', users.recent);
 
-app.post('/users', checkForDrinks, function(req, res){
-  if(req.body.drink){
-    User.findOne({name: req.body.drink.to}, function(err, user){
-      if(err) throw err;
-      if(user){
-        user.drinks.push({type: req.body.drink.type, from: req.body.drink.from});
-        user.save(function(err){
-        if(err) throw err;
-          res.send(user);
-        });
-      }else{
-        var d = new Date();
-        d.setDate(d.getDate()-1);
-        user = new User({name: req.body.drink.to, lastDrink: d});
-        user.drinks.push({type: req.body.drink.type, from: req.body.drink.from});
-        user.save(function(err){
-        if(err) throw err;
-          res.send(user);
-        });
-      }
-    });
-  }else{
-    res.send({'error': 'looks like you\'re missing some stuff'})
-  }
-});
-app.post('/dan',function(req, res){
-  dan = new User({name: 'Dan Nawara'});
-  dan.drinks.push({type: 'beer', from: 'Dan Nawara'});
-  dan.save(function(er){
-    if(!er){
-      res.send("created dan\n")
-    }
-  });
-})
+app.get('/users', users.list);
+
+app.post('/users', users.checkForDrinks, users.create);
+
+app.get('/users/:name', users.show);
+
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
---
+++ routes/users.js
@@ -0,0 +1,102 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/beer');
+
+var Schema = mongoose.Schema;
+
+var Drinks = new Schema({
+  type: { type: String, required: true}
+, from: { type: String, requred: true }
+});
+
+var User = new Schema({  
+  name:       { type: String, required: true, unique: true}
+, drinks:     [Drinks]
+, email:      { type: String, requred: true }
+, lastDrink:  { type: Date, default: Date.now }
+, updatedAt:  { type: Date, default: Date.now }
+});
+
+
+var User = mongoose.model('User', User);
+/*
+ * GET home page.
+ */
+var Beer = {
+  index: function(req, res){
+    User.find({}, function(err, users){
+      res.render('index', { title: 'Drinks owed.', users: users });
+    });
+  }
+, list: function(req, res){
+    User.find({}, function(err, users){
+      res.send(users);
+    });
+  }
+, show: function(req, res){
+    User.findOne({name: req.params.name}, function(err, user){
+      res.send(user);
+    });
+  }
+, recent: function(req, res){
+    User.find({$where: "this.drinks.length > 0"}, function(err, recent){
+      if(err) throw err;
+      res.send(recent);
+    });
+  }
+, create: function(req, res){
+    if(req.body.drink){
+      User.findOne({name: req.body.drink.to}, function(err, user){
+        if(err) throw err;
+        if(user){
+          user.drinks.push({type: req.body.drink.type, from: req.body.drink.from});
+          user.updatedAt = new Date();
+          user.save(function(err){
+          if(err) throw err;
+            res.send(user);
+          });
+        }else{
+          var d = new Date();
+          d.setDate(d.getDate()-1);
+          user = new User({name: req.body.drink.to, lastDrink: d, email: req.body.drink.email});
+          user.drinks.push({type: req.body.drink.type, from: req.body.drink.from});
+          user.save(function(err){
+          if(err) throw err;
+            res.send(user);
+          });
+        }
+      });
+    }else{
+      res.send({'error': 'looks like you\'re missing some stuff'})
+    }
+  }
+, checkForDrinks: function(req, res, next){
+    User.findOne({name: req.body.drink.from}, function(er, pal){
+      if(er) throw er;
+      console.log(pal);
+      if(pal == null){
+        next(new Error("That's not a user"));
+      }
+      var hasDrinksToGive = function(){
+        var today = new Date();
+        var lastbeer = new Date(pal.lastDrink);
+        console.log(lastbeer, today);
+        if(lastbeer.getMonth() == today.getMonth() && lastbeer.getDate() == today.getDate()){
+          return false;
+        }else{
+          return true;
+        }
+      }
+      if(pal && hasDrinksToGive()){
+        pal.lastDrink = new Date();
+        pal.save(function(er){
+          next();
+        });
+      }else{
+        next(new Error('User Has no drinks to give.'));
+      }
+    });
+  }
+};
+
+module.exports= Beer;
+
